924cc89e96a1b6ece90a8359db879e33fa569837
RavenHogWarts <fengyj120@163.com>
2024-03-25 17:08:47 +0800

    Manual Update

--- main.ts
+++ main.ts
@@ -1,134 +1,177 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CustomIconSettings {
+    customIcons: Array<{label: string; svgData: string;}>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CustomIconSettings = {
+    customIcons: []
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CustomIconPlugin extends Plugin {
+    settings: CustomIconSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // 注册标签变化时刷新图标的事件
+        this.registerEvent(
+            this.app.workspace.on('layout-change', () => {
+                this.refreshIcons();
+            })
+        );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // 添加设置标签
+        this.addSettingTab(new CustomIconSettingTab(this.app, this));
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    onunload() {
+        // 插件卸载时的清理逻辑
+        console.log('Custom Icon Plugin unloaded');
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    private encodeSvgData(svgData: string): string {
+        // 移除SVG的width和height属性
+        svgData = svgData.replace(/width="[^"]*"|height="[^"]*"/g, '');
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // 处理并编码SVG数据为base64 URI
+        const encodedSvgData = unescape(encodeURIComponent(svgData));
+        return `data:image/svg+xml;base64,${window.btoa(encodedSvgData)}`;
+    }
 
-	onunload() {
+    // 刷新图标的方法
+    refreshIcons() {
+        // 清除之前的样式以防重复添加
+        const styleEl = document.getElementById('custom-icon-styles');
+        styleEl?.remove();
 
-	}
+        // 创建样式元素
+        const newStyleEl = document.createElement('style');
+        newStyleEl.setAttribute('id', 'custom-icon-styles');
+        let css = '';
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+        // 遍历所有自定义图标设置
+        this.settings.customIcons.forEach(icon => {
+            const svgBase64 = this.encodeSvgData(icon.svgData);
+            css += `
+                .workspace-tab-header[aria-label="${icon.label}"] .workspace-tab-header-inner-icon > svg {
+                    display: none;
+                }
+                .workspace-tab-header[aria-label="${icon.label}"] .workspace-tab-header-inner-icon::before {
+                    content: " ";
+                    display: inline-block;
+                    width: 1em;
+                    height: 1em;
+                    background-color: var(--text-normal);
+                    -webkit-mask-image: url('${svgBase64}');
+                    mask-image: url('${svgBase64}');
+                }
+            `;
+        });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        // 将样式添加到文档头部
+        newStyleEl.textContent = css;
+        document.head.appendChild(newStyleEl);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// 这里将会是一个设置界面的实现，用户可以上传和设置自定义图标
+// ...
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class CustomIconSettingTab extends PluginSettingTab {
+    plugin: CustomIconPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    constructor(app: App, plugin: CustomIconPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
+
+        containerEl.createEl('h2', { text: '自定义图标设置' });
+
+        // 图标设置列表
+        this.plugin.settings.customIcons.forEach((icon, index) => {
+            const iconSetting = new Setting(containerEl)
+                .setName(`图标 #${index + 1}`)
+                .setDesc('请输入SVG的XML内容');
+
+            iconSetting.addText(text =>
+                text
+                    .setValue(icon.label)
+                    .setPlaceholder('图标名称')
+                    .onChange(async (value) => {
+                        icon.label = value;
+                        await this.plugin.saveSettings();
+                        this.plugin.refreshIcons();
+                    })),
+            iconSetting.addTextArea(textArea =>
+                textArea
+                    .setValue(icon.svgData)
+                    .setPlaceholder('在这里粘贴SVG内部元素')
+                    .onChange(async (value) => {
+                        icon.svgData = value;
+                        await this.plugin.saveSettings();
+                        this.plugin.refreshIcons();
+                    })
+            );
+            iconSetting.addButton(button =>
+                button
+                    .setButtonText('移除')
+                    .setCta()
+                    .onClick(async () => {
+                        this.plugin.settings.customIcons.splice(index, 1);
+                        await this.plugin.saveSettings();
+                        this.display();
+                    })
+            );
+        });
+
+        // 添加图标按钮
+        new Setting(containerEl)
+            .addButton(button =>
+                button
+                    .setButtonText('添加新图标')
+                    .onClick(async () => {
+                        this.plugin.settings.customIcons.push({
+                            label: '',
+                            svgData: ''
+                        });
+                        await this.plugin.saveSettings();
+                        this.display();
+                    })
+            );
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function encodeURIComponentWithNonASCII(str: string): string {
+    const asciiPattern = /[!'()*]/g;
+    return encodeURIComponent(str).replace(asciiPattern, c =>
+        '%' + c.charCodeAt(0).toString(16).toUpperCase()
+    );
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+function svgToBase64(svgString: string): string {
+    // 移除SVG标签中不必要的属性，比如width和height
+    svgString = svgString
+        .replace(/width="[^"]*"/g, '')
+        .replace(/height="[^"]*"/g, '');
 
-	display(): void {
-		const {containerEl} = this;
+    // 使用encodeURIComponentWithNonASCII函数进行编码
+    const encodedSVG = encodeURIComponentWithNonASCII(svgString);
 
-		containerEl.empty();
+    // 转换为base64格式
+    const base64SVG = btoa(encodedSVG);
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    return base64SVG;
 }

0e8979d43672ee391030ec75070e38974f2b7687
RavenHogWarts <fengyj120@163.com>
2024-04-10 19:39:51 +0800

    Update SVG XML content label in en.ts

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -4,7 +4,7 @@ export const EN: LocalProperty = {
   settings: "Add custom entry icon",
   settingsDesc: "Supported image formats: Online URL or relative file path or base64 encoding or svg tag",
   iconLabel: "Icon #{num}",
-  svgXmlContent: "Enter SVG XML content",
+  svgXmlContent: "Enter svg xml content",
   fileNamePlaceholder: "Enter file name",
   imagePlaceholder: "Enter icon Path",
   removeButton: "Remove",
